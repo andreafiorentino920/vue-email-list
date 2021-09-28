bba723da43edbd78d2cf6e76dbd334ef91703b0b
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-09-28 11:15:07 +0100

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 const app = new Vue({
     el:"#root",
     data:{
-
+        emails:[]
     },
     methods:{
-        
-    }
+    },
+    mounted(){
+        for(var i = 1; i <=10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
+                    this.emails.push(response.data.response)
+                    });
+        }            
+    }    
 })
+console.log(this.emails)
